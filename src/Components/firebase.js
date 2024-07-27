e5b5d2b154fc78d3ac1923ed4affe7d15ee91e57
Vedant <vedants372@gmail.com>
2024-07-27 18:32:47 +0530

    upd

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,3 +1,4 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -6,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCakMhPSb5JFeycZHvxSPDB_ujxrACCboI",
-  authDomain: "learning-edu-app.firebaseapp.com",
-  projectId: "learning-edu-app",
-  storageBucket: "learning-edu-app.appspot.com",
-  messagingSenderId: "718001890315",
-  appId: "1:718001890315:web:f068897e285a457e8a130e",
-  measurementId: "G-178HMBYJ58"
+  apiKey: "AIzaSyA0Aj0MO_RC9mTJe-BVbZvCX7at0anTi5Q",
+  authDomain: "eduorbit-ccbb0.firebaseapp.com",
+  projectId: "eduorbit-ccbb0",
+  storageBucket: "eduorbit-ccbb0.appspot.com",
+  messagingSenderId: "493346251096",
+  appId: "1:493346251096:web:61b1e728fbef8dc8ec052d",
+  measurementId: "G-3Z55HV17C6"
 };
 
 // Initialize Firebase
